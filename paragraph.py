cc303632550cd7de4f22559f20eb8b8b9159536d
Bart Massey <bart.massey@gmail.com>
2020-11-20 17:56:16 -0800

    commented

--- paragraph.py
+++ paragraph.py
@@ -1,14 +1,20 @@
 import os, re, sys
 from collections import defaultdict
 
+# Regexes for processing.
 name = re.compile("[a-z] +([A-Z][a-zA-Z]+)")
 punct = re.compile("[^-' a-zA-Z]+")
 dash = re.compile("--")
 possess = re.compile("'s")
 
+# novel should be an iterable text object.  Returns the
+# processed text of novel as a string.
 def process(novel):
-
+    # List of paragraphs. Each paragraph is a list of string
+    # lines, not newline-terminated.
     pars = list()
+
+    # Accumulate paragraphs, filtering out extraneous lines.
     par = list()
     for line in novel:
         words = line.split()
@@ -27,15 +33,15 @@ def process(novel):
     if par and len(par) > 1:
         pars.append(par)
 
+    # Accumulate names by count of occurrences.
     pnames = defaultdict(int)
-
     for p in pars:
         text = ' '.join(p)
         for g in name.finditer(text):
             pnames[g.group(1)] += 1
 
+    # Construct the set of names.
     propnames = {n for n in pnames if pnames[n] >= 2}
-
     propnames -= {
         "Sir",
         "Miss",
@@ -47,7 +53,8 @@ def process(novel):
         "Lady",
     }
 
-    def clean_words(line):
+    # Substitute names.
+    def clean_names(line):
         line = dash.sub(" ", line)
         line = punct.sub(" ", line)
         cleaned = line.split()
@@ -59,13 +66,15 @@ def process(novel):
                 cleaned[i] = "—'s"
         return " ".join(cleaned) + "\n"
 
+    # Build up the paragraphs with the names cleaned.
     return "\n".join([
         ''.join([
-            clean_words(l) for l in p
+            clean_names(l) for l in p
         ])
         for p in pars
     ])
 
+# Process all available novels.
 for fn in os.listdir("hacked/"):
     with open(f"hacked/{fn}", "r") as f_in:
         text = process(f_in)
